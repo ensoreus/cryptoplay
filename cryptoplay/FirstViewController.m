da6d7909121ed89b1ebafe3e92164b8284fbdf0a
Phillip Maluta <phillip.maluta@volia-software.com>
2016-11-11 13:06:47 +0200

    Initial Commit

---
+++ cryptoplay/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  cryptoplay
+//
+//  Created by Philipp Maluta on 11.11.16.
+//  Copyright © 2016 com.ensoreus. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ cryptoplay/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  cryptoplay
+//
+//  Created by Philipp Maluta on 11.11.16.
+//  Copyright © 2016 com.ensoreus. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
