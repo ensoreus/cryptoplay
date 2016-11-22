8e43c109f5fee3fa8d18f4d387eed2421ac6a41c
Phillip Maluta <phillip.maluta@volia-software.com>
2016-11-22 20:57:18 +0200

    changed tasks list

--- cryptoplay/AesRsaViewController.m
+++ cryptoplay/AesRsaViewController.m
@@ -24,15 +24,20 @@ int envelope_seal(EVP_PKEY **pub_key, unsigned char *plaintext, int plaintext_le
 int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_len,
                   unsigned char *encrypted_key, int encrypted_key_len, unsigned char *iv,
                   unsigned char *plaintext);
+int aesEncrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+               unsigned char *iv, unsigned char *ciphertext);
 
 @interface AesRsaViewController ()
 @property(nonatomic, weak) IBOutlet UILabel* generatedLine;
+@property (weak, nonatomic) IBOutlet UITextView *aesKey;
+
 @property(nonatomic, weak) IBOutlet UITextView* encryptedLine;
+@property (weak, nonatomic) IBOutlet UITextView *encryptedAesKey;
 @property(nonatomic, weak) IBOutlet UILabel* decryptedLine;
 @end
 
 @implementation AesRsaViewController{
-    EVP_PKEY_CTX * kctx;
+    //EVP_PKEY_CTX * kctx;
     unsigned char* iv;
     unsigned char* ekey;
     int ekeyLength;
@@ -56,9 +61,17 @@ int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.generatedLine.text = [self generateString];
+    NSData* aesKey = [self generateAesKey];
+    self.encryptedLine.text = [self aesEncryptText:self.generatedLine.text withAes:aesKey];
+    NSMutableString* sAesKey = [NSMutableString stringWithCapacity:256];
+    unsigned char* cAesKey = malloc(256);
+    [aesKey getBytes:cAesKey range:NSMakeRange(0, 256)];
+    for (int i = 0; i < 256; i++) {
+        [sAesKey appendFormat:@"%02x", cAesKey[i]];
+    }
+    self.aesKey.text = sAesKey;
     EVP_PKEY* key_pair = [self generateRsaKeyPair];
-    [self sealEnvelope:self.generatedLine.text keyPair:key_pair];
-    [self decryptString:self.encryptedLine.text keyPair:key_pair initVector:iv];
+    [self sealEnvelope:sAesKey keyPair:key_pair];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -77,6 +90,16 @@ int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_
     CRYPTO_cleanup_all_ex_data();
     ERR_free_strings();
 }
+
+/*
+ 1 encrypt plaintext with AES
+ 2 encrypt AES key with RSA public key
+ 3 store AES key to keychain
+ 
+ show AES & plaintext
+ show encrypted AES and enctext
+ 
+ */
 
 - (void) sealEnvelope:(NSString*)str keyPair:(EVP_PKEY*)key_pair{
     
@@ -97,36 +120,57 @@ int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_
     ekeyLength = encryptedKeySize;
     ekey = encrypted_key;
     [self storeKeys:key_pair initVector:iv];
-    self.encryptedLine.text = resStr;
+    self.encryptedAesKey.text = resStr;
     
 }
 
-- (void) decryptString:(NSString*)str keyPair:(EVP_PKEY*)keyPair initVector:(unsigned char*)initVector{
-    EVP_PKEY* privateKey = EVP_PKEY_new();
-    int pkeyLen;
-    unsigned char *ucBuf, *uctempBuf;
-    pkeyLen = i2d_PrivateKey(keyPair, NULL);
-    ucBuf = (unsigned char *)malloc(pkeyLen+1);
-    uctempBuf = ucBuf;
-    i2d_PrivateKey(keyPair, &uctempBuf);
-    EVP_PKEY_assign(privateKey, EVP_PKEY_RSA, ucBuf);
+- (NSData*) generateAesKey{
     
-    unsigned char cStr[BUFSIZE];
-    strncpy((char*)cStr, [str cStringUsingEncoding:NSASCIIStringEncoding], str.length);
-    unsigned char *decrypted = malloc(2048);
-    int decrlen = envelope_open(keyPair, encryptedText, encryptedTextLength, ekey, ekeyLength, initVector, decrypted);
-    NSString* strDec = [NSString stringWithCString:decrypted encoding:NSASCIIStringEncoding];
-    self.decryptedLine.text = strDec;
+    uint8_t *aesKey = calloc(256, sizeof(uint8_t));
+    int res = SecRandomCopyBytes(kSecRandomDefault, 256, aesKey);
+    if (res != 0) {
+        NSLog(@"Error aesKey: %d", errno);
+        return 0;
+    }else{
+        NSData* dAesKey = [NSData dataWithBytes:aesKey length:256];
+        return dAesKey;
+    }
 }
+
+- (NSString*) aesEncryptText:(NSString*)plainText withAes:(NSData*)aesKey{
+    unsigned char* plaintext = malloc(plainText.length + 1);
+    uint16_t usedPlainTextLength = 0;
+    strncpy(plaintext, [plainText cStringUsingEncoding:NSASCIIStringEncoding], plainText.length);
+    
+    unsigned char* generatedAesKey = malloc(256);
+    [aesKey getBytes:generatedAesKey range:NSMakeRange(0, 256)];
+    unsigned char* _iv = malloc(128);
+    int ivGenRes = SecRandomCopyBytes(kSecRandomDefault, 128, _iv);
+    unsigned char* ciphertext = malloc(512);
+    int cipherSize = aesEncrypt(plaintext, usedPlainTextLength, generatedAesKey, _iv, ciphertext);
+    NSMutableString* resStr = [NSMutableString stringWithCapacity:cipherSize];
+    for (int i = 0; i < cipherSize; i++) {
+        [resStr appendFormat:@"%02x", ciphertext[i]];
+    }
+    free(plaintext);
+    
+    return resStr;
+}
+
+
+//- (NSString*) encryptAesKey:(NSData*)aesKey{
+//    
+//}
 
 - (NSString*) generateString {
     return [[NSUUID UUID] UUIDString];
 }
 
 - (EVP_PKEY*) generateRsaKeyPair{
-    if (kctx != NULL){
-        EVP_PKEY_CTX_free(kctx);
-    }
+    EVP_PKEY_CTX * kctx;
+//    if (kctx != NULL){
+//        EVP_PKEY_CTX_free(kctx);
+//    }
     kctx = NULL;
     
     int type = EVP_PKEY_RSA;
@@ -153,10 +197,16 @@ int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_
         ERR_print_errors_fp(stderr);
         return NULL;
     }
+    EVP_PKEY_CTX_free(kctx);
     return key;
 }
 
 // Task #2
+
+- (void) createKeychain{
+   
+}
+
 - (void) storeKeys:(EVP_PKEY*)keyPair initVector:(unsigned char*)iv{
     NSData* secKeyData = [self extractPrivateKeyData:keyPair];
     CFErrorRef error = NULL;
@@ -169,7 +219,7 @@ int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_
     NSDictionary * attributes = @{
                                   (id)kSecUseItemList: @[secKeyData],
                                   (id)kSecClass: (id)kSecClassKey,
-                                  (id)kSecAttrLabel:@"com.ensoreus.cryptoplay.skey",
+                                  (id)kSecAttrLabel: @"com.ensoreus.cryptoplay.skey",
                                   (id)kSecAttrKeyClass: (id)kSecAttrKeyClassPrivate,
                                   (id)kSecAttrAccessControl: (__bridge id)acl,
                                   (id)kSecAttrKeySizeInBits: @256,
@@ -185,6 +235,25 @@ int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_
         NSLog(@"Error!!!");
     }
 }
+
+- (void) decryptString:(NSString*)str keyPair:(EVP_PKEY*)keyPair initVector:(unsigned char*)initVector{
+    EVP_PKEY* privateKey = EVP_PKEY_new();
+    int pkeyLen;
+    unsigned char *ucBuf, *uctempBuf;
+    pkeyLen = i2d_PrivateKey(keyPair, NULL);
+    ucBuf = (unsigned char *)malloc(pkeyLen+1);
+    uctempBuf = ucBuf;
+    i2d_PrivateKey(keyPair, &uctempBuf);
+    EVP_PKEY_assign(privateKey, EVP_PKEY_RSA, ucBuf);
+    
+    unsigned char cStr[BUFSIZE];
+    strncpy((char*)cStr, [str cStringUsingEncoding:NSASCIIStringEncoding], str.length);
+    unsigned char *decrypted = malloc(2048);
+    int decrlen = envelope_open(keyPair, encryptedText, encryptedTextLength, ekey, ekeyLength, initVector, decrypted);
+    NSString* strDec = [NSString stringWithCString:decrypted encoding:NSASCIIStringEncoding];
+    self.decryptedLine.text = strDec;
+}
+
 
 - (NSString*) extractPublicKey:(EVP_PKEY*)keyPair{
     int pkeyLen;
@@ -347,4 +416,55 @@ int envelope_open(EVP_PKEY *priv_key, unsigned char *ciphertext, int ciphertext_
     EVP_CIPHER_CTX_free(ctx);
     
     return plaintext_len;
+}
+
+int aesEncrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
+    
+    int len;
+    
+    int ciphertext_len;
+    
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) {
+        ERR_print_errors_fp(stderr);
+        return 0;
+    }
+    
+    /* Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+    {
+        ERR_print_errors_fp(stderr);
+        return 0;
+    }
+    
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+    {
+        ERR_print_errors_fp(stderr);
+        return 0;
+    }
+    ciphertext_len = len;
+    
+    /* Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)){
+        ERR_print_errors_fp(stderr);
+        return 0;
+    }
+    ciphertext_len += len;
+    
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+    
+    return ciphertext_len;
 }
